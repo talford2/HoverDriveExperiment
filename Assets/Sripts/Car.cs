c655316797449a98ec8d424dabe5a16c73a61376
talford2 <talford2@hotmail.com>
2014-08-31 01:20:47 +1000

    Initial commit.

---
+++ Assets/Sripts/Car.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    public WheelCollider FrontLeftCollider;
+    public WheelCollider FrontRightCollider;
+    public WheelCollider RearLeftCollider;
+    public WheelCollider RearRightCollider;
+
+    public Transform WheelFrontLeft;
+    public Transform WheelFrontRight;
+    public Transform WheelRearLeft;
+    public Transform WheelRearRight;
+
+
+    private float steer;
+
+    private float speed;
+    private float maxSpeed = 100;
+    private float acceleration = 100f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        //Debug.Log("2");
+
+        //void Update () {
+
+        //steer = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            steer = Mathf.LerpAngle(steer, -30f, Time.deltaTime);
+            //steer = -30;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            steer = Mathf.LerpAngle(steer, 30f, Time.deltaTime);
+            //steer = 30;
+        }
+
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            speed += acceleration * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            speed -= acceleration * Time.deltaTime;
+
+        }
+        speed = Mathf.Clamp(speed, maxSpeed * -1, maxSpeed);
+        //else
+        //{
+        //    if (speed>0)
+        //    {
+        //        speed -= acceleration * Time.deltaTime;
+        //    }
+        //    if (speed<0)
+        //    {
+        //        speed += acceleration * Time.deltaTime;
+        //    }
+        //}
+
+        //Debug.Log("Speed : " + speed);
+
+        FrontLeftCollider.motorTorque = speed;
+        FrontRightCollider.motorTorque = speed;
+
+        FrontLeftCollider.steerAngle = steer;
+        FrontRightCollider.steerAngle = steer;
+
+        /*
+        WheelFrontLeft.localEulerAngles = new Vector3(WheelFrontLeft.localEulerAngles.x, steer + 90f, (WheelFrontLeft.localEulerAngles.z));
+        WheelFrontRight.localEulerAngles = new Vector3(WheelFrontRight.localEulerAngles.x, steer + 90f, (WheelFrontRight.localEulerAngles.z));
+        */
+
+        WheelFrontLeft.Rotate(0, FrontLeftCollider.rpm * 6 * Time.deltaTime, 0);
+        WheelFrontRight.Rotate(0, FrontRightCollider.rpm * 6 * Time.deltaTime, steer);
+        WheelRearLeft.Rotate(0, RearLeftCollider.rpm * 6 * Time.deltaTime, 0);
+        WheelRearRight.Rotate(0, RearRightCollider.rpm * 6 * Time.deltaTime, 0);
+
+        //WheelFrontLeft.localEulerAngles = new Vector3(WheelFrontLeft.localEulerAngles.x, steer, WheelFrontLeft.localEulerAngles.z);
+        //WheelFrontRight.localEulerAngles = new Vector3(WheelFrontRight.localEulerAngles.x, steer, WheelFrontRight.localEulerAngles.z);
+    }
+}
---
+++ Assets/Sripts/HoverVehicle.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class HoverVehicle : MonoBehaviour
+{
+    public Transform FrontLeftSphere;
+    public Transform FrontRightSphere;
+
+    public Transform LeftSphere;
+    public Transform RightSphere;
+
+    public Transform CentreSphere;
+
+    void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        var hoverHeight = 5f;
+
+        var up = Vector3.up;// new Vector3(0, 1f, 0);
+
+
+        var down = Vector3.down;// new Vector3(0, -1, 0);
+
+        var frontLeftPoint = new Vector3(-2, 0, 2f);
+        var frontRightPoint = new Vector3(2, 0, 2f);
+
+        var leftPoint = new Vector3(-2f, 0, -2);
+        var rightPoint = new Vector3(2f, 0, -2);
+
+        var centre = new Vector3(0, 0, 0);
+
+        FrontLeftSphere.localPosition = frontLeftPoint;
+        FrontRightSphere.localPosition = frontRightPoint;
+
+        LeftSphere.localPosition = leftPoint;
+        RightSphere.localPosition = rightPoint;
+
+        CentreSphere.localPosition = centre;
+
+        RaycastHit frontLeftHit;
+        RaycastHit frontRightHit;
+        RaycastHit rearLeftHit;
+        RaycastHit rearRightHit;
+        RaycastHit centreHit;
+
+        Physics.Raycast(FrontLeftSphere.position, down, out frontLeftHit);
+        Physics.Raycast(FrontRightSphere.position, down, out frontRightHit);
+        Physics.Raycast(LeftSphere.position, down, out rearLeftHit);
+        Physics.Raycast(RightSphere.position, down, out rearRightHit);
+        Physics.Raycast(centre, down, out centreHit);
+
+
+
+
+        var angleHelperForce = 0.1f;
+
+        var frontLeftLift = -Physics.gravity.y * angleHelperForce * (hoverHeight - frontLeftHit.distance * 2);
+        var frontRightLift = -Physics.gravity.y * angleHelperForce * (hoverHeight - frontRightHit.distance * 2);
+        var leftLift = -Physics.gravity.y * angleHelperForce * (hoverHeight - rearLeftHit.distance * 2);
+        var rightLift = -Physics.gravity.y * angleHelperForce * (hoverHeight - rearRightHit.distance * 2);
+
+        var centreLift = -Physics.gravity.y * (hoverHeight - centreHit.distance);
+
+
+        var boost = 150f;
+
+        GetComponentInChildren<Rigidbody>().AddForceAtPosition(up * frontLeftLift * boost, FrontLeftSphere.position);
+        GetComponentInChildren<Rigidbody>().AddForceAtPosition(up * frontRightLift * boost, FrontRightSphere.position);
+        GetComponentInChildren<Rigidbody>().AddForceAtPosition(up * leftLift * boost, LeftSphere.position);
+        GetComponentInChildren<Rigidbody>().AddForceAtPosition(up * rightLift * boost, RightSphere.position);
+
+        //GetComponentInChildren<Rigidbody>().AddForce(up * centreLift);
+
+        GetComponentInChildren<Rigidbody>().AddRelativeForce(GetComponentInChildren<Rigidbody>().transform.up * centreLift * boost);
+
+
+        //GetComponentInChildren<Rigidbody>().AddForceAtPosition(up * centreLift, centre);
+    }
+
+    void Update()
+    {
+    }
+}
